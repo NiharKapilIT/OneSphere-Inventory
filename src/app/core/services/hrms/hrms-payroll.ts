65b4bcb8a402d754129e74940dc4a268fa3e86cc
shaiktalha-u <shaiktalha@kapilit.com>
2026-05-06 11:09:36 +0530

    khc-detials ,general recipet view

--- src/app/core/services/hrms/hrms-payroll.ts
+++ src/app/core/services/hrms/hrms-payroll.ts
@@ -1,6 +1,28 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { CommonService } from "../Common/common.service";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root",
 })
-export class HrmsPayroll {}
+export class HrmsPayroll {
+ constructor(private http: HttpClient, private _CommonService: CommonService) { }
+
+
+
+  getPolicyEmployeeDetails(GlobalSchema: any, CompanyCode: any, BranchSchema: any, BranchId: any): Observable<any> {
+    const params = new HttpParams().set('GlobalSchema', GlobalSchema) .set('CompanyCode', CompanyCode).set('BranchSchema', BranchSchema)
+     .set('BranchId', BranchId);
+    return this._CommonService.getAPI('/Accounts/getPolicyEmployeeDetails', params, 'YES');
+  }
+    
+
+
+    SavePolicyDetails(data: any) {
+    return this._CommonService.postAPI('/Accounts/SavePolicyDetails', data);
+  }
+
+
+
+}
