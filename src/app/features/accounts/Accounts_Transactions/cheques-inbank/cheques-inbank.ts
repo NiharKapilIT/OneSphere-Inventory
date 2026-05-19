75b41dfd2b4e4d2fc1a6a5956b21f1d7a537043e
RishikaPatel Kandukuri <rishikak@kapilit.com>
2026-05-19 10:17:34 +0530

    cheques in bank

--- src/app/features/accounts/Accounts_Transactions/cheques-inbank/cheques-inbank.ts
+++ src/app/features/accounts/Accounts_Transactions/cheques-inbank/cheques-inbank.ts
@@ -1489,7 +1489,9 @@ export class ChequesInbank implements OnInit {
 
   private _prepareSaveItems(items: any[], isAuto: boolean) {
     for (const item of items) {
-      item.pCreatedby = '1';
+      //item.pCreatedby = '1';
+      
+      item.pCreatedby = sessionStorage.getItem('userId');
       item.pdepositeddate = this._commonService.getFormatDateNormal(
         this._commonService.getDateObjectFromDataBase(item.pdepositeddate));
       item.preceiptdate = this._commonService.getFormatDateNormal(
